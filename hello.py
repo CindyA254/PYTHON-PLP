735c09035f7246574fe535e5f9eb13eec292bbf1
Cindy <143493823+CindyA254@users.noreply.github.com>
2024-03-26 16:43:18 +0300

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,13 @@
+# Introduction
+# Setting Up of the environment
+# First program
+# variables
+# Data types
+# Numbers
+# Operations
+print(2+2)
+print("I am printing through PLP for the first time")
+first_name = "mutemi" #string
+second_name = "wambui" #string
+age = 60 #integer
+worth = 100.00 #double
---
+++ quiz.py
@@ -0,0 +1,17 @@
+# What will be the output of the following Python code?
+
+list1 = [3 , 2 , 5 , 6 , 0 , 7, 9]
+
+sum = 0
+
+sum1 = 0
+
+for elem in list1:
+
+if (elem % 2 == 0):
+sum = sum + elem
+if (elem % 3 == 0):
+
+sum1 = sum1 + elem
+
+print(sum , end=" ")
